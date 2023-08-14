2cb69d77ad081262c722adb4453ac413e6322aae
saratikyan <91123731+54R4T1KY4N@users.noreply.github.com>
2023-08-15 02:17:52 +0400

    gptBLEND.sh

--- gptBLEND.sh
+++ gptBLEND.sh
@@ -2,51 +2,6 @@
 
 echo "ok, let's blend..."
 
-banner1() {
-  local text="$@"
-  local length=$(( ${#text} + 2 ))
-  local line=$(printf '%*s' "$length" '' | tr ' ' '-')
-  echo "+$line+"
-  printf "| %s |\n" "$(date)"
-  echo "+$line+"
-  printf "|$bold%s$reset|\n" "$text"
-  echo "+$line+"
-}
-
-
-#clolors
-white='\e[1;37m'
-green='\e[0;32m'
-blue='\e[1;34m'
-red='\e[1;31m'
-yellow='\e[1;33m' 
-echo ""
-echo ""
-banner() {
-	echo -e $'\e[1;33m\e[0m\e[1;37m       ▄████  ██▓███  ▄▄▄█████▓ ▄▄▄▄    ██▓    ▓█████  ███▄    █ ▓█████▄    \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m      ██▒ ▀█▒▓██░  ██▒▓  ██▒ ▓▒▓█████▄ ▓██▒    ▓█   ▀  ██ ▀█   █ ▒██▀ ██▌   \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m     ▒██░▄▄▄░▓██░ ██▓▒▒ ▓██░ ▒░▒██▒ ▄██▒██░    ▒███   ▓██  ▀█ ██▒░██   █▌   \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m     ░▓█  ██▓▒██▄█▓▒ ▒░ ▓██▓ ░ ▒██░█▀  ▒██░    ▒▓█  ▄ ▓██▒  ▐▌██▒░▓█▄   ▌   \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m     ░▒▓███▀▒▒██▒ ░  ░  ▒██▒ ░ ░▓█  ▀█▓░██████▒░▒████▒▒██░   ▓██░░▒████▓    \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m      ░▒   ▒ ▒▓▒░ ░  ░  ▒ ░░   ░▒▓███▀▒░ ▒░▓  ░░░ ▒░ ░░ ▒░   ▒ ▒  ▒▒▓  ▒    \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m       ░   ░ ░▒ ░         ░    ▒░▒   ░ ░ ░ ▒  ░ ░ ░  ░░ ░░   ░ ▒░ ░ ▒  ▒    \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m     ░ ░   ░ ░░         ░       ░    ░   ░ ░      ░      ░   ░ ░  ░ ░  ░    \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m           ░                    ░          ░  ░   ░  ░         ░    ░       \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m                                     ░                            ░         \e[0m'
-	
-	
-	echo""    
-	echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m blender with GPT \e[0m''\e[1;37m\e[0m\e[1;37m \e[0m'                                       
-	echo ""
-	echo -e $'\e[1;33m\e[0m\e[1;33m  [ \e[0m\e[1;32m Follow on Github :- https://github.com/54R4T1KY4N \e[0m \e[1;32m\e[0m\e[1;33m] \e[0m'
-	echo ""
-	echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+ >>\e[0m'
-	echo -e "\e[93m    gptBLEND |1|.|3| stable"      
-	echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+ >>\e[0m' 
-	echo ""                                                
-}
-banner 
-
 # Set log file path
 LOG_FILE="blender_script.log"
 
@@ -61,7 +16,7 @@ for dependency in $dependencies; do
 done
 
 if [ ${#missing_dependencies[@]} -gt 0 ]; then
-    banner1 "Error: The following dependencies are missing: ${missing_dependencies[@]}"
+    echo "Error: The following dependencies are missing: ${missing_dependencies[@]}"
     read -p "Would you like to install them now? (y/n) " install_deps
 
     if [[ "$install_deps" == "y" ]]; then
@@ -85,27 +40,27 @@ if [ ${#missing_dependencies[@]} -gt 0 ]; then
             sudo apk add "${missing_dependencies[@]}"
             pip install openai torch transformers
         else
-            banner1 "Error: Could not detect supported package manager. Please install the missing dependencies manually."
+            echo "Error: Could not detect supported package manager. Please install the missing dependencies manually."
             exit 1
         fi
     else
-        banner1 "Error: The missing dependencies must be installed to run this script. Please install them manually."
+        echo "Error: The missing dependencies must be installed to run this script. Please install them manually."
         exit 1
     fi
 fi
 
 # How to use this damn script
-banner1 "This script generates Python code for Blender using the ChatGPT API."
-banner1 "To use this script, enter a prompt that describes the Python code you want to generate."
-banner1 "For best results, use natural language and be as specific as possible."
-banner1 'For example: "Generate Python code to create a 3D model of a car in Blender"' | tee -a "$LOG_FILE"
+echo "This script generates Python code for Blender using the ChatGPT API."
+echo "To use this script, enter a prompt that describes the Python code you want to generate."
+echo "For best results, use natural language and be as specific as possible."
+echo 'For example: "Generate Python code to create a 3D model of a car in Blender"' | tee -a "$LOG_FILE"
 
 # Prompt the user for input
 read -p "Enter a prompt: " prompt
 
 # Validate the input prompt
 if [ -z "$prompt" ]; then
-    banner1 "Error: Prompt cannot be empty. Please try again." | tee -a "$LOG_FILE"
+    echo "Error: Prompt cannot be empty. Please try again." | tee -a "$LOG_FILE"
     exit 1
 fi
 
@@ -117,7 +72,7 @@ code="$response"
 
 # Validate the generated code
 if [ -z "$code" ]; then
-    banner1 "Error: Generated code is empty. Please try a different prompt." | tee -a "$LOG_FILE"
+    echo "Error: Generated code is empty. Please try a different prompt." | tee -a "$LOG_FILE"
     exit 1
 fi
 
@@ -127,4 +82,4 @@ echo "$code" > blend.py
 # Run the blender command
 blender -b -P blend.py
 
-banner1 "Blending complete."
+echo "Blending complete."
